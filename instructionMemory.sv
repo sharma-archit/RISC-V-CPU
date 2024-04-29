fcb5ab5c134cd038ccf94283d341aa16f510aa5b
sharma-archit <archit.sharma.live@gmail.com>
2024-04-29 17:39:17 -0600

    Updated comments

--- instructionMemory.sv
+++ instructionMemory.sv
@@ -7,7 +7,7 @@ logic [2^31-1:0][7:0] intruction_memory; // instruction memory is half the addre
 
 always_comb begin
 
-    for (int i=0; i < 4 ; i++) begin
+    for (int i=0; i < 4 ; i++) begin //Transfer 8 bit data sets in 4 steps, totalling 32 bits
 
         instruction[8*i+7:8*i] = instruction_memory[addr + i]; 
             
