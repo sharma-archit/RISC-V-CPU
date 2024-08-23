ebe58cf99e6a4bc316088f9e97924fd726c1d4c4
sharma-archit <archit.sharma.live@gmail.com>
2024-08-23 17:58:32 -0600

    Created script to simplify inputting instruction binaries in testbench file

--- cpuCore_tb.sv
+++ cpuCore_tb.sv
@@ -11,30 +11,6 @@ const time CLK_PERIOD = 8;
 cpuCore cpu_core(.*);
 
     initial begin
-        rst = 1;
-        dbg_wr_en = 0;
-        dbg_addr = 0;
-        dbg_instr = {12'b000000001100, 5'b00000, 3'b000, 5'b00001, 7'b0010011}; //immediate value = 12, source register = 0, dest reg = 1, opcode = ADDI
-        #(2*CLK_PERIOD)
-        dbg_wr_en = 1;
-        #CLK_PERIOD
-        dbg_wr_en = 0;
-        #CLK_PERIOD
-        dbg_addr = 4;
-        dbg_instr = {12'b000000000010, 5'b00000, 3'b000, 5'b00010, 7'b0010011}; //immediate value = 2, source register = 0, dest reg = 2, opcode = ADDI
-        #(2*CLK_PERIOD)
-        dbg_wr_en = 1;
-        #CLK_PERIOD
-        dbg_wr_en = 0;
-        #CLK_PERIOD
-        dbg_addr = 8;
-        dbg_instr = {1'b0, 6'b000000, 5'b00000, 5'b00001, 3'b101, 4'b1100, 1'b0, 7'b1100011}; //immediate value = 12, source reg 2 = 0, source reg 1 = 2, dest reg = 3, opcode = BGE
-        #(2*CLK_PERIOD)
-        dbg_wr_en = 1;
-        #CLK_PERIOD
-        dbg_wr_en = 0;
-        #(2*CLK_PERIOD)
-        rst = 0;
 
     end
     
